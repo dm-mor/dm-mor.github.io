55af065ab802f3ca833d777e501a1be95d661e98
dm-mor <155527236+dm-mor@users.noreply.github.com>
2024-07-05 18:48:33 +0700

    Update jsset.js

--- script/jsset.js
+++ script/jsset.js
@@ -1,4 +1,16 @@
 setTheme('theme-dark');
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(97755859, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
 document.ondragstart = test;
 //запрет на перетаскивание 
 document.onselectstart = test;
